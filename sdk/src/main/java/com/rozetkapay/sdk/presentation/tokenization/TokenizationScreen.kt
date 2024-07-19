d366b32b0fdf673d603314dc46fa37452ee03b4f
nestoleh <nestoleh@gmail.com>
2024-07-19 12:33:14 +0300

    UI fixes

--- sdk/src/main/java/com/rozetkapay/sdk/presentation/tokenization/TokenizationScreen.kt
+++ sdk/src/main/java/com/rozetkapay/sdk/presentation/tokenization/TokenizationScreen.kt
@@ -13,6 +13,7 @@ import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.text.input.ImeAction
+import androidx.compose.ui.text.input.KeyboardCapitalization
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import com.rozetkapay.sdk.R
@@ -81,7 +82,10 @@ private fun TokenizationContent(
                 onValueChange = {
                     onNameChanged(it)
                 },
-                keyboardOptions = KeyboardOptions(imeAction = ImeAction.Next),
+                keyboardOptions = KeyboardOptions(
+                    imeAction = ImeAction.Next,
+                    capitalization = KeyboardCapitalization.Sentences
+                ),
             )
         }
         Spacer(modifier = Modifier.height(28.dp))
@@ -94,7 +98,7 @@ private fun TokenizationContent(
         Spacer(modifier = Modifier.height(40.dp))
         PrimaryButton(
             modifier = Modifier.fillMaxWidth(),
-            text = "Save",
+            text = stringResource(id = R.string.rozetka_pay_tokenization_save_button),
             onClick = onSave
         )
     }
